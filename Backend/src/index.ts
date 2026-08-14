bddc5c652bc7472c040dff58e8e302b07f919ecf
Sahil Sharma <sahilsharma901590@gmail.com>
2026-08-14 18:52:14 +0530

    Connect the Db

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,5 +1,8 @@
 const app=require('./app')
+const connectDB=require('./config/db')
+
+connectDB();
 
 app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port 3000")
 })
