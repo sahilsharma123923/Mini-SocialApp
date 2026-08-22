3f4e402089e7adf9d5c70442faae5c69a369c52d
Sahil Sharma <sahilsharma901590@gmail.com>
2026-08-22 22:21:15 +0530

    Add new features to edit and delete comment

--- Frontend/src/store/PostStore.ts
+++ Frontend/src/store/PostStore.ts
@@ -5,17 +5,19 @@ import type { Comment } from "@/types/comments";
 
 interface PostStore {
   posts: Post[];
-   comments: Comment[];
+  comments: Comment[];
   addPost: (newPost: Post) => void;
   toggleLiked: (id: number) => void;
-  addComment:(comment:Comment)=>void;
-  deletePost:(id:number)=>void;
-  editPost:(id:number,content:string)=>void;
+  addComment: (comment: Comment) => void;
+  deletePost: (id: number) => void;
+  editPost: (id: number, content: string) => void;
+  editComment: (id: number, content: string) => void;
+  deleteComment: (id: number) => void;
 }
 
 export const usePostStore = create<PostStore>((set) => ({
   posts: fakePosts,
-  comments:[],
+  comments: [],
   addPost: (newPost) => {
     set((state) => ({
       posts: [newPost, ...state.posts],
@@ -38,33 +40,69 @@ export const usePostStore = create<PostStore>((set) => ({
     }));
   },
 
-  addComment:(comment)=>{
-    set((state)=>({
-      comments:[...state.comments,comment],
-      posts:state.posts.map((post)=>
-      post.id===comment.postId ?{
-        ...post,
-        comments:post.comments+1
-      }
-    :post
-    )
+  addComment: (comment) => {
+    set((state) => ({
+      comments: [...state.comments, comment],
+      posts: state.posts.map((post) =>
+        post.id === comment.postId
+          ? {
+              ...post,
+              comments: post.comments + 1,
+            }
+          : post
+      ),
     }));
   },
- 
-  deletePost:(id)=>{
-    set((state)=>({
-      posts:state.posts.filter((post)=>post.id !==id)
-    }))
+
+  deletePost: (id) => {
+    set((state) => ({
+      posts: state.posts.filter((post) => post.id !== id),
+    }));
   },
+
   editPost(id, content) {
-    set((state)=>({
-      posts:state.posts.map((post)=>
-        post.id===id ? {
-          ...post,
-          content:content,
-        }
-        :post
-      )
-    }))
+    set((state) => ({
+      posts: state.posts.map((post) =>
+        post.id === id
+          ? {
+              ...post,
+              content: content,
+            }
+          : post
+      ),
+    }));
+  },
+
+  editComment: (id, content) => {
+    set((state) => ({
+      comments: state.comments.map((comment) =>
+        comment.id === id
+          ? {
+              ...comment,
+              content: content,
+            }
+          : comment
+      ),
+    }));
+  },
+
+  deleteComment: (id) => {
+    set((state) => {
+      const commentToDelete = state.comments.find(
+        (comment) => comment.id === id
+      );
+
+      return {
+        comments: state.comments.filter((comment) => comment.id !== id),
+        posts: state.posts.map((post) =>
+          post.id === commentToDelete?.postId
+            ? {
+                ...post,
+                comments: post.comments - 1,
+              }
+            : post
+        ),
+      };
+    });
   },
 }));
